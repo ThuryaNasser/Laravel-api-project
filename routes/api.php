9478dd6151957734458455e238baf6843002681d
ThuryaNasser <63455773+ThuryaNasser@users.noreply.github.com>
2023-01-16 12:15:44 +0300

    auth contoller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,20 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('/register', [AuthController::class, 'createUser']);
+Route::post('/login', [AuthController::class, 'login']);
 
-
-
-// Route::post('login', [LoginController::class, 'login']);
-
-Route::prefix('posts')->group(function () {
-    // Route::middleware('auth:sanctum')->prefix('posts')->group(function () {
+Route::middleware('auth:sanctum')->prefix('posts')->group(function () {
     Route::get('', [PostController::class, 'index']);
     Route::post('', [PostController::class, 'store']);
     Route::get('/{id}', [PostController::class, 'show']);
     Route::put('/{post}', [PostController::class, 'update']);
     Route::delete('/{post}', [PostController::class, 'destroy']);
 });
-
